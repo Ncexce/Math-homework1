56ed80df92b7c8c6c7136e9fc2a7bd9325f399e5
Ncexce <1825578362@qq.com>
2020-02-24 12:50:32 +0800

    1st generation

---
+++ Mainhand.R
@@ -0,0 +1,28 @@
+#Created on 2020/2/24,9:17
+#Data Initialize
+source=data.frame(x=1:10,y=c(15152,5093,2644,2009,2051,1891,1751,825,892,399))
+print(source)
+sect1=data.frame(x=1:4,y=c(15152,5093,2644,2009))
+sect2=data.frame(x=4:7,y=c(2009,2051,1891,1751))
+sect3=data.frame(x=7:10,y=c(1751,825,892,399))
+#Regression Calc
+fs1=lm(y~x,sect1)
+fs2=lm(y~x,sect2)
+fs3=lm(y~x,sect3)
+#Export graph plots.svg
+svg("plots.svg")
+plot(source,xlim=c(1,12))
+abline(a=fs1$coefficients[1],b=fs1$coefficients[2],col="red")
+abline(a=fs2$coefficients[1],b=fs2$coefficients[2],col="green")
+abline(a=fs3$coefficients[1],b=fs3$coefficients[2],col="blue")
+dev.off()
+#Print the graph on the screen
+plot(source,xlim=c(1,12))
+abline(a=fs1$coefficients[1],b=fs1$coefficients[2],col="red")
+abline(a=fs2$coefficients[1],b=fs2$coefficients[2],col="green")
+abline(a=fs3$coefficients[1],b=fs3$coefficients[2],col="blue")
+#Solve the function(y=0) && Export
+f=function(x, a, b){return(a*x+b)}
+root=uniroot(f,c(10,12),a=fs3$coefficients[2],b=fs3$coefficients[1],tol=0.1)
+root$root
+# Will vanish after 11 days.
